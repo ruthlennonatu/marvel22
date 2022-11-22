e260bce379ca1b505b2f6f0204c3777d61d007bc
Shane.Russel <115080503+L00151942@users.noreply.github.com>
2022-11-22 19:10:15 +0000

    maven files added

---
+++ atulabs/mavenghactions/src/main/java/atulabs/ie/mavenghactions/App.java
@@ -0,0 +1,13 @@
+package atulabs.ie.mavenghactions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
